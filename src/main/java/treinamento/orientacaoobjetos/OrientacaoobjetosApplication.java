69a8c25a7b32db0d49680ba18c2bcc9fecfad054
Eric Guilherme <eric.guilherme@bluesoft.com.br>
2020-10-06 15:48:27 -0300

    #Iniciando projeto OO

---
+++ src/main/java/treinamento/orientacaoobjetos/OrientacaoobjetosApplication.java
@@ -0,0 +1,13 @@
+package treinamento.orientacaoobjetos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrientacaoobjetosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrientacaoobjetosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/treinamento/orientacaoobjetos/OrientacaoobjetosApplicationTests.java
@@ -0,0 +1,13 @@
+package treinamento.orientacaoobjetos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrientacaoobjetosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
